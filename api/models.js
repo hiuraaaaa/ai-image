a589279d422a3e6e287b6f9dff706f97ab2b1754
Hiura <hiura0012@gmail.com>
2025-12-15 02:50:26 +0700

    Update models.js

--- api/models.js
+++ api/models.js
@@ -1,11 +1,6 @@
-import models from './_lib/models'
+import { getAllModels } from './_lib/models'
 
 export default function handler(req, res) {
-  const list = Object.values(models).map(m => ({
-    id: m.meta.id,
-    name: m.meta.name,
-    sizes: m.meta.sizes
-  }))
-
-  res.status(200).json(list) // langsung array
+  const list = getAllModels()
+  res.status(200).json(list) // langsung array, frontend bisa pakai langsung
 }
