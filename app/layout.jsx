f4a88fda8d4dd7c6a08aaca75e0367e9ac6d4f34
Hiura <hiura0012@gmail.com>
2025-12-15 00:15:58 +0700

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'AI Image SaaS',
+  description: 'Text to Image Generator'
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
