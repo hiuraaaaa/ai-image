726e77dcb177e2e11ee63dbdb82222b92fc11893
Hiura <hiura0012@gmail.com>
2025-12-15 00:21:21 +0700

    Create models.js
    hi

---
+++ api/models.js
@@ -0,0 +1,11 @@
+import models from './_lib/models'
+
+export default function handler(req, res) {
+  const list = Object.values(models).map(m => ({
+    id: m.meta.id,
+    name: m.meta.name,
+    sizes: m.meta.sizes
+  }))
+
+  res.status(200).json({ success: true, data: list })
+}
