90326c16a76cb5f2233c9d4eb025435c1f9cb6f3
Hiura <hiura0012@gmail.com>
2025-12-15 01:45:15 +0700

    Update index.js

--- api/_lib/models/index.js
+++ api/_lib/models/index.js
@@ -1,18 +1,9 @@
-import fs from 'fs'
-import path from 'path'
+import registry from './registry'
 
-const modelsDir = path.join(process.cwd(), 'api/_lib/models')
-
-const models = {}
-
-for (const folder of fs.readdirSync(modelsDir)) {
-  if (folder === 'index.js') continue
-
-  const modelPath = path.join(modelsDir, folder, 'model.js')
-  if (fs.existsSync(modelPath)) {
-    const mod = await import(`./${folder}/model.js`)
-    models[mod.meta.id] = mod
-  }
+export function getAllModels() {
+  return registry
 }
 
-export default models
+export function getModelById(id) {
+  return registry.find(m => m.id === id)
+}
