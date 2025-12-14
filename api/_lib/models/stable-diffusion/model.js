9c7ddbc250f54205a236ccb4c9832d2307b822e7
Hiura <hiura0012@gmail.com>
2025-12-15 02:13:45 +0700

    Update model.js

--- api/_lib/models/stable-diffusion/model.js
+++ api/_lib/models/stable-diffusion/model.js
@@ -1,17 +1,14 @@
-import axios from 'axios'
-
-export const meta = {
-  id: 'stable-diffusion',
-  name: 'Stable Diffusion 3.5',
-  sizes: ['1:1', '9:16', '16:9']
+const model = {
+  meta: {
+    id: 'stable-diffusion',
+    name: 'Stable Diffusion 3.5',
+    sizes: ['1:1','9:16','16:9']
+  },
+  generate: async ({ prompt, ratio }) => {
+    const url = 'https://api.nekolabs.web.id/image-generation/stable-diffusion/3.5'
+    const res = await axios.get(url, { params: { prompt, ratio } })
+    return res.data.result
+  }
 }
 
-export async function generate({ prompt, ratio }) {
-  const url = 'https://api.nekolabs.web.id/image-generation/stable-diffusion/3.5'
-
-  const res = await axios.get(url, {
-    params: { prompt, ratio }
-  })
-
-  return res.data.result
-}
+export default model
